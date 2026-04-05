b7e9652e1a26eebea3fcc07c07247ec20921176b
carlo <carlo@Andres>
2026-04-04 18:19:46 -0600

    Posible clase de analizador semántico

---
+++ src/AnalizadorSemantico/Analizadorsem.java
@@ -0,0 +1,90 @@
+package AnalizadorSemantico;
+
+import generated.LenguajeBaseVisitor;
+import generated.LenguajeParser;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author carlo
+ */
+public class Analizadorsem extends LenguajeBaseVisitor<Double> {
+
+    Map<String, Double> tabla = new HashMap<>();
+
+    public Analizadorsem(JTextArea terminal) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Double visitDeclaracion(LenguajeParser.DeclaracionContext ctx) {
+        String id = ctx.ID().getText();
+
+        if (tabla.containsKey(id)) {
+            System.out.println("Error: variable ya declarada " + id);
+            return 0.0;
+        }
+
+        double valor = 0.0;
+
+        if (ctx.e != null) {
+            valor = visit(ctx.e);
+        }
+
+        tabla.put(id, valor);
+        return 0.0;
+    }
+
+    @Override
+    public Double visitAsignacion(LenguajeParser.AsignacionContext ctx) {
+        String id = ctx.ID().getText();
+
+        if (!tabla.containsKey(id)) {
+            System.out.println("Error: variable no declarada " + id);
+            return 0.0;
+        }
+
+        double valor = visit(ctx.expresion());
+        tabla.put(id, valor);
+
+        return valor;
+    }
+
+    @Override
+    public Double visitSuma(LenguajeParser.SumaContext ctx) {
+        double result = visit(ctx.mult(0));
+
+        for (int i = 1; i < ctx.mult().size(); i++) {
+            if (ctx.MAS(i - 1) != null) {
+                result += visit(ctx.mult(i));
+            } else {
+                result -= visit(ctx.mult(i));
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public Double visitPrimario(LenguajeParser.PrimarioContext ctx) {
+        if (ctx.NUMERO() != null) {
+            return Double.parseDouble(ctx.NUMERO().getText());
+        }
+
+        if (ctx.ID() != null) {
+            String id = ctx.ID().getText();
+
+            if (!tabla.containsKey(id)) {
+                System.out.println("Error: variable no definida " + id);
+                return 0.0;
+            }
+
+            return tabla.get(id);
+        }
+
+        return visit(ctx.expresion());
+    }
+
+}
